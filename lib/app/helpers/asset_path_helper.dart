dbde319ad56e7decde3a66093996a5100cfbac63
ikindred <kindred.inocencio@gmail.com>
2024-03-02 06:38:01 +0800

    update slash

--- lib/app/helpers/asset_path_helper.dart
+++ lib/app/helpers/asset_path_helper.dart
@@ -1,5 +1,6 @@
 class AssetPath {
   static const _imageMainPath = 'assets/images';
+  static const _imageSvgPath = 'assets/svg';
 
   // Introduction
   static const introBg = '$_imageMainPath/intro_bg.png';
@@ -8,4 +9,7 @@ class AssetPath {
   static const onboarding1 = '$_imageMainPath/onboarding1.png';
   static const onboarding2 = '$_imageMainPath/onboarding2.png';
   static const onboarding3 = '$_imageMainPath/onboarding3.png';
+
+  // Splash
+  static const mynthLogo = '$_imageSvgPath/mynth_logo.svg';
 }
